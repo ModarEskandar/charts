5902dca10dd1c7c19b7733a55097a027a3199c6e
Modar Eskandar <modar.a.eskandar@gmail.com>
2023-04-17 00:53:26 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,219 @@
 import { Component } from '@angular/core';
+import { ChartOptions } from './charts/charts.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'charts';
+  open = true;
+  authedUser = 'مضر اسكندر';
+  jobTitle = 'موظف الهيئة العامة للجمارك';
+  isAuthenticated = true;
+  chartOptions: Partial<ChartOptions>[] = [
+    {
+      title: {
+        text: 'Product Trends by Month',
+        align: 'center',
+      },
+      series: [
+        {
+          name: 'distibuted',
+          data: [21, 22, 10, 28, 16, 21, 13, 30],
+        },
+      ],
+      chart: {
+        height: 350,
+        type: 'bar',
+        events: {
+          click: function (chart, w, e) {
+            // console.log(chart, w, e)
+          },
+        },
+      },
+      colors: [
+        '#008FFB',
+        '#00E396',
+        '#FEB019',
+        '#FF4560',
+        '#775DD0',
+        '#546E7A',
+        '#26a69a',
+        '#D10CE8',
+      ],
+      plotOptions: {
+        bar: {
+          columnWidth: '45%',
+          distributed: true,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      legend: {
+        show: false,
+      },
+      grid: {
+        show: false,
+      },
+      xaxis: {
+        categories: [
+          ['John', 'Doe'],
+          ['Joe', 'Smith'],
+          ['Jake', 'Williams'],
+          'Amber',
+          ['Peter', 'Brown'],
+          ['Mary', 'Evans'],
+          ['David', 'Wilson'],
+          ['Lily', 'Roberts'],
+        ],
+        labels: {
+          style: {
+            colors: [
+              '#008FFB',
+              '#00E396',
+              '#FEB019',
+              '#FF4560',
+              '#775DD0',
+              '#546E7A',
+              '#26a69a',
+              '#D10CE8',
+            ],
+            fontSize: '12px',
+          },
+        },
+      },
+    },
+    {
+      series: [
+        {
+          name: 'Desktops',
+          data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
+        },
+      ],
+      chart: {
+        height: 350,
+        type: 'line',
+        zoom: {
+          enabled: false,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: 'straight',
+      },
+      title: {
+        text: 'Product Trends by Month',
+        align: 'left',
+      },
+      grid: {
+        row: {
+          colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+          opacity: 0.5,
+        },
+      },
+      xaxis: {
+        categories: [
+          'Jan',
+          'Feb',
+          'Mar',
+          'Apr',
+          'May',
+          'Jun',
+          'Jul',
+          'Aug',
+          'Sep',
+        ],
+      },
+    },
+    {
+      title: {
+        text: 'Social Media Apps Usage',
+      },
+      seriesNa: [76, 67, 61, 90],
+      chart: {
+        height: 390,
+        type: 'radialBar',
+      },
+      plotOptions: {
+        radialBar: {
+          offsetY: 0,
+          startAngle: 0,
+          endAngle: 270,
+          hollow: {
+            margin: 5,
+            size: '30%',
+            background: 'transparent',
+            image: undefined,
+          },
+          dataLabels: {
+            name: {
+              show: false,
+            },
+            value: {
+              show: false,
+            },
+          },
+        },
+      },
+      colors: ['#1ab7ea', '#0084ff', '#39539E', '#0077B5'],
+      labels: ['Vimeo', 'Messenger', 'Facebook', 'LinkedIn'],
+      legend: {
+        show: true,
+        floating: true,
+        fontSize: '16px',
+        position: 'left',
+        offsetX: 50,
+        offsetY: 10,
+        labels: {
+          useSeriesColors: true,
+        },
+        formatter: function (seriesName, opts) {
+          return seriesName + ':  ' + opts.w.globals.series[opts.seriesIndex];
+        },
+        itemMargin: {
+          horizontal: 3,
+        },
+      },
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            legend: {
+              show: false,
+            },
+          },
+        },
+      ],
+    },
+    {
+      series: [
+        {
+          name: 'Series 1',
+          data: [80, 90, 99, 80, 45, 78],
+        },
+      ],
+      chart: {
+        height: 350,
+        type: 'radar',
+      },
+      title: {
+        text: 'CR7 Fifa Rate Chart',
+      },
+      xaxis: {
+        categories: [
+          'Pace',
+          'Shooting',
+          'Passing',
+          'Dribbling',
+          'Defending',
+          'Physical',
+        ],
+      },
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
+import { ChartsComponent } from './charts/charts.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ChartsComponent],
+  imports: [BrowserModule, NgApexchartsModule, MatIconModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
